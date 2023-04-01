9530c9ce1c8ffa8380d2c03acfceff4e21a4d781
reddicka <yu.nailka.rav@yandex.ru>
2023-04-01 12:31:20 +0500

    create login, register components

---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import AddAvatar from '../img/addAvatar.png'
+
+
+export const Login = () => {
+    return (
+        <div className='formContainer'>
+            <div className="formWrapper">
+                <span className='logo'>Chat</span>
+                <span className='title'>Login</span>
+
+                <form>
+                    <input type="email" placeholder='Email'/>
+                    <input type="password" placeholder='Password'/>
+
+                    <button>Sign in</button>
+                </form>
+
+                <p>You don't have an account? Register</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,29 @@
+import AddAvatar from '../img/addAvatar.png'
 
 
 export const Register = () => {
     return (
-        <p>qwe</p>
+        <div className='formContainer'>
+            <div className="formWrapper">
+                <span className='logo'>Chat</span>
+                <span className='title'>Register</span>
+
+                <form>
+                    <input type="text" placeholder='Nickname'/>
+                    <input type="email" placeholder='Email'/>
+                    <input type="password" placeholder='Password'/>
+
+                    <label htmlFor="file">
+                        <img src={AddAvatar} alt="Add file image"/>
+                        <span>Add an avatar</span>
+                    </label>
+                    <input style={{display: "none"}} type="file" id='file'/>
+
+                    <button>Sign up</button>
+                </form>
+
+                <p>You do have an account? Login</p>
+            </div>
+        </div>
     )
 }
